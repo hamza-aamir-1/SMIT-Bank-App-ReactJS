85d9862f9bc8202a686db9348a6bf863d1a00c54
Hamza Aamir <hamzaamirm2001@gmail.com>
2022-05-10 19:31:30 +0500

    Update Timer.jsx

--- src/Components/Timer/Timer.jsx
+++ src/Components/Timer/Timer.jsx
@@ -3,7 +3,7 @@ import './Timer.css'
 
 const Timer = () => {
 
-    // const [myDate, setMyDate] = useState(Date());
+    const [myDate, setMyDate] = useState(Date());
     // const myDate = Date();
 
 // var today = new Date();
@@ -11,38 +11,44 @@ const Timer = () => {
 // let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 // let dateTime = date+' '+time;
 
-// const handleTimer = () => {
-//     let timer = new Date();
-//     let year = timer.getFullYear();
-//     let month = timer.getMonth() + 1;
-//     let date = timer.getDate();
-//     let day = timer.getDay();
-//     let seconds = timer.getSeconds();
-//     let showTimer = year + ' : ' + month + ' : ' + date + ' : ' + day + ' : ' + seconds;
-//     let myDiv = document.getElementById(timebox);
-//     return(
-//         // console.log(showTimer);
-//         myDiv.innerText = showTimer
-//     )
-//     // setTimeout("handleTimer()",100);
-// }
-
+const handleTimer = () => {
     let timer = new Date();
     let year = timer.getFullYear();
     let month = timer.getMonth() + 1;
     let date = timer.getDate();
     let day = timer.getDay();
-    let hour = timer.getHours();
-    let minute = timer.getMinutes();
-    const seconds = () => {
-        // setTimeout('seconds()',100)
-        return timer.getSeconds()
-    }
-    let showTimer = month + ', ' + date + 'th ' + year + ', ' + hour + ':' + minute + ':' + seconds();
+    let seconds = timer.getSeconds();
+    setMyDate(year + ' : ' + month + ' : ' + date + ' : ' + day + ' : ' + seconds);
+    // console.log(showTimer);
+// return showTimer;
+    // let myDiv = document.getElementById(timebox);
+    // return(
+    //     // console.log(showTimer);
+    //     myDiv.innerText = showTimer
+    // )
+    // setTimeout("handleTimer()",100);
+}
 
+    // let timer = new Date();
+    // let year = timer.getFullYear();
+    // let month = timer.getMonth() + 1;
+    // let date = timer.getDate();
+    // let day = timer.getDay();
+    // let hour = timer.getHours();
+    // let minute = timer.getMinutes();
+    // const seconds = () => {
+    //     // setTimeout('seconds()',100)
+    //     return timer.getSeconds()
+    // }
+    // let showTimer = month + ', ' + date + 'th ' + year + ', ' + hour + ':' + minute + ':' + seconds();
+
+    // setTimeout(handleTimer, 100)
+    setInterval(() => {
+        handleTimer()
+    }, 1000);
 return (
     <div className='timer-box' id='timebox'>
-        {showTimer}
+        {myDate}
     </div>
 )
 }
